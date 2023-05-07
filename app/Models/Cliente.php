c6c4c4ed582113a670b05ea60daadac6a2507036
GABRIELLY MALDONADO DORNELES <49543498+GMDorneles@users.noreply.github.com>
2023-05-07 19:08:59 -0400

    first commit

---
+++ app/Models/Cliente.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    use HasFactory;
+    protected $table = 'cliente';
+    protected $fillable = [
+        'nome',
+        'sexo',
+        'cpf',
+        'contato',
+        'endereço'
+    ];
+}
---
+++ database/migrations/2023_05_06_210822_create_cliente_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100)->nullable();
+            $table->string('sexo', 20)->nullable();
+            $table->integer('cpf')->nullable();
+            $table->string('contato', 30)->nullable();
+            $table->string('endereço', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cliente');
+    }
+};
